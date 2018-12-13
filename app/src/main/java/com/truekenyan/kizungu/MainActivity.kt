5731c8ab3dff01d9aa7a5f337103911789da1a02
Kevin-Kip <masterfork5@gmail.com>
2018-12-13 08:24:34 +0300

    Added textwatcher

--- app/src/main/java/com/truekenyan/kizungu/MainActivity.kt
+++ app/src/main/java/com/truekenyan/kizungu/MainActivity.kt
@@ -5,21 +5,55 @@ import android.os.Build
 import android.os.Bundle
 import android.speech.tts.TextToSpeech
 import android.support.v7.app.AppCompatActivity
+import android.text.Editable
+import android.text.TextWatcher
 import android.util.Log
 import android.view.Menu
 import android.view.MenuItem
+import android.view.View
 import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
 import java.util.*
 
 class MainActivity : AppCompatActivity(), TextToSpeech.OnInitListener {
 
     private var textToSpeech: TextToSpeech? = null
+    private var text: String? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        setSupportActionBar(toolbar)
+
         textToSpeech = TextToSpeech(this@MainActivity, this)
+        initListeners()
+    }
+
+    private fun initListeners(){
+        button_search.setOnClickListener {
+            text = input_word.text.trim().toString()
+        }
+
+        button_clear.setOnClickListener {
+            input_word.text.clear()
+        }
+
+        input_word.addTextChangedListener(object : TextWatcher{
+            override fun afterTextChanged(s: Editable?) {}
+
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+                button_search.isEnabled = count > 0
+            }
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                if (count > 0) {
+                    button_clear.visibility = View.VISIBLE
+                } else {
+                    button_clear.visibility = View.INVISIBLE
+                }
+            }
+        })
     }
 
     override fun onInit(status: Int) {
@@ -32,13 +66,15 @@ class MainActivity : AppCompatActivity(), TextToSpeech.OnInitListener {
                 // TODO enable speak button
             }
         } else {
-            Log.e("TTS", "Initilization Failed!")
+            Log.e("TTS", "Initialization Failed!")
         }
     }
 
-    private fun speak(text: String){
+    private fun speak(text: String?){
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
-            textToSpeech?.speak(text, TextToSpeech.QUEUE_FLUSH, null, "")
+            if (text != null) {
+                textToSpeech?.speak(text, TextToSpeech.QUEUE_FLUSH, null, "")
+            }
         }
     }
 
@@ -49,6 +85,7 @@ class MainActivity : AppCompatActivity(), TextToSpeech.OnInitListener {
 
     override fun onOptionsItemSelected(item: MenuItem?): Boolean {
         when (item?.itemId){
+            R.id.options_history -> startActivity(Intent(this@MainActivity, HistoryActivity::class.java))
             R.id.options_settings -> startActivity(Intent(this@MainActivity, SettingsActivity::class.java))
         }
         return super.onOptionsItemSelected(item)

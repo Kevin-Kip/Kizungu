d702a8e3d95224a7de142b85c2739ed15395aa5e
Kevin-Kip <masterfork5@gmail.com>
2018-12-23 11:24:15 +0300

    Initialized history activity

--- app/src/main/java/com/truekenyan/kizungu/activities/HistoryActivity.kt
+++ app/src/main/java/com/truekenyan/kizungu/activities/HistoryActivity.kt
@@ -2,17 +2,34 @@ package com.truekenyan.kizungu.activities
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.support.v7.widget.DefaultItemAnimator
+import android.support.v7.widget.LinearLayoutManager
 import android.view.Menu
 import android.view.MenuItem
 import com.truekenyan.kizungu.R
+import com.truekenyan.kizungu.adapters.HistoryAdapter
+import com.truekenyan.kizungu.interfaces.ClickListener
+import com.truekenyan.kizungu.models.Word
+import kotlinx.android.synthetic.main.activity_history.*
 
-class HistoryActivity : AppCompatActivity() {
+class HistoryActivity : AppCompatActivity(), ClickListener {
+
+    private var historyAdapter: HistoryAdapter? = null
+    private val wordList = mutableListOf<Word>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_history)
         setSupportActionBar(toolbar)
         supportActionBar!!.setDisplayHomeAsUpEnabled(true)
+
+        historyAdapter = HistoryAdapter(wordList)
+        history.apply {
+            hasFixedSize()
+            adapter = historyAdapter
+            layoutManager = LinearLayoutManager(this@HistoryActivity)
+            itemAnimator = DefaultItemAnimator()
+        }
     }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
@@ -22,8 +39,12 @@ class HistoryActivity : AppCompatActivity() {
 
     override fun onOptionsItemSelected(item: MenuItem?): Boolean {
         when (item?.itemId) {
-
+            R.id.clear_history -> historyAdapter!!.clearItems()
         }
         return true
+    }
+
+    override fun onItemCLicked(wordId: Int?) {
+
     }
 }
